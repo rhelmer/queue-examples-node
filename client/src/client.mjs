a3e17a4d2026d58660082239016979412d41a441
Robert Helmer <robert@roberthelmer.com>
2023-04-11 20:08:53 -0700

    working cloudtask client example

--- client/src/client.mjs
+++ client/src/client.mjs
@@ -1,1 +1,41 @@
-import { CloudTasksClient } from "@google-cloud/tasks"
+// Based on https://github.com/aertje/cloud-tasks-emulator#javascript-example
+
+import { CloudTasksClient } from '@google-cloud/tasks';
+import { credentials } from '@grpc/grpc-js';
+
+const client = new CloudTasksClient({
+  port: 8123,
+  servicePath: 'localhost',
+  sslCreds: credentials.createInsecure(),
+});
+
+const serviceAccountEmail = "PROJECT_ID@appspot.gserviceaccount.com";
+const parent = 'projects/my-sandbox/locations/us-central1';
+const queueName = `${parent}/queues/test`;
+
+const payload = {
+  foo: "bar",
+  key: "value"
+};
+
+const queue = await client.getQueue({ name: queueName })
+if (!queue) {
+  await client.createQueue({ parent, queue: { name: queueName } });
+}
+
+await client.createTask({
+  parent: queueName,
+  task: {
+    httpRequest: {
+      httpMethod: 'POST',
+      url: 'https://f09e-2605-59c8-3091-d710-2dca-a845-3b04-f811.ngrok-free.app/api/v1/process',
+      body: Buffer.from(JSON.stringify(payload)).toString("base64"),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      oidcToken: {
+        serviceAccountEmail
+      }
+    }
+  },
+});
